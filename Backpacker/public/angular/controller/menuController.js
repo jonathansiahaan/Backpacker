b423db8da10d886c26c260a485641cc754ca8c35
jonathansiahaan <jonathan.siahaan@justcommodity.com>
2015-12-08 13:19:53 +0700

    Ini yg baru

---
+++ Backpacker/public/angular/controller/menuController.js
@@ -0,0 +1,60 @@
+'use strict';
+
+/* Controllers */
+function MenuCtrl($scope, $http, $templateCache) {
+	$scope.showModal = false;
+    $scope.toggleModal = function(){
+        $scope.showModal = !$scope.showModal;
+    };
+    
+  $scope.test="TEST";
+  var method = 'POST';
+  var inserturl = 'http://localhost:1212/insertangularmongouser';// URL where the Node.js server is running
+  $scope.codeStatus = "";
+  $scope.save = function() {
+	  
+    // Preparing the Json Data from the Angular Model to send in the Server. 
+    var formData = {
+      'username' : this.username,
+      'password' : this.password,
+	  'email' : this.email
+    };
+
+	this.username = '';
+	this.password = '';
+	this.email = '';
+
+	var jdata = 'mydata='+JSON.stringify(formData); // The data is to be string.
+
+	$http({ // Accessing the Angular $http Service to send data via REST Communication to Node Server.
+            method: method,
+            url: inserturl,
+            data:  jdata ,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            cache: $templateCache
+        }).
+        success(function(response) {
+		console.log("success"); // Getting Success Response in Callback
+                $scope.codeStatus = response.data;
+		console.log($scope.codeStatus);
+
+        }).
+        error(function(response) {
+			console.log("error"); // Getting Error Response in Callback
+	        $scope.codeStatus = response || "Request failed";
+			console.log($scope.codeStatus);
+        });
+		$scope.list();// Calling the list function in Angular Controller to show all current data in HTML
+        return false;
+  };	
+
+  $scope.list = function() {
+	  var url = 'http://localhost:6060/getangularusers';// URL where the Node.js server is running	
+	  $http.get(url).success(function(data) {
+		$scope.users = data;
+	  });
+          // Accessing the Angular $http Service to get data via REST Communication from Node Server 
+  };
+
+  $scope.list();
+}
---
+++ Backpacker/public/angular/module.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var myApp = angular.module('myApp', ['ngRoute']); // Taking Angular Application in Javascript Variable
+
+//Below is the code to allow cross domain request from web server through angular.js
+myApp.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+
+]);
+
+
+myApp.controller('MainCtrl', function ($scope) {
+    $scope.showModal = false;
+    $scope.toggleModal = function(){
+        $scope.showModal = !$scope.showModal;
+    };
+    $scope.test="Belum";
+    $scope.addTrip = function(){
+    	$scope.test="Udah";
+    	var url = 'http://localhost:6060/add_trip';
+    	$http.post(url,$scope.formData).
+        success(function(data) {
+            console.log("posted successfully");
+        }).error(function(data) {
+            console.error("error in posting");
+        })
+    }
+});
+
+
+myApp.directive('modal', function () {
+    return {
+      template: '<div class="modal fade">' + 
+          '<div class="modal-dialog">' + 
+            '<div class="modal-content">' + 
+              '<div class="modal-header">' + 
+                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' + 
+                '<h4 class="modal-title">{{ title }}</h4>' + 
+              '</div>' + 
+              '<div class="modal-body" ng-transclude></div>' + 
+            '</div>' + 
+          '</div>' + 
+        '</div>',
+      restrict: 'E',
+      transclude: true,
+      replace:true,
+      scope:true,
+      link: function postLink(scope, element, attrs) {
+        scope.title = attrs.title;
+
+        scope.$watch(attrs.visible, function(value){
+          if(value == true)
+            $(element).modal('show');
+          else
+            $(element).modal('hide');
+        });
+
+        $(element).on('shown.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = true;
+          });
+        });
+
+        $(element).on('hidden.bs.modal', function(){
+          scope.$apply(function(){
+            scope.$parent[attrs.visible] = false;
+          });
+        });
+      }
+    };
+  });
